2e3888d59668daef6849f692856e52291b617506
Deniskinyua <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@github.com>
2022-03-12 16:29:11 +0300

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-/**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main () {
+    char Str1 [] = "Programming is like building a multilingual puzzle";
+    puts(Str1);
 }
