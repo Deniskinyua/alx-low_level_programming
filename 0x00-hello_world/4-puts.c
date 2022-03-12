c47b6f9ce92cec0684bdb4674821dce103752355
Deniskinyua <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@github.com>
2022-03-12 16:35:09 +0300

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main () {
-    char Str1 [] = "Programming is like building a multilingual puzzle";
+    char Str1 [] = ""Programming is like building a multilingual puzzle";
     puts(Str1);
     return 0;
 }
