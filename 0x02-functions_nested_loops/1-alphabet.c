278f4a284e03308f976907010ed428378712c020
Deniskinyua <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@github.com>
2022-03-15 23:57:14 +0300

    Insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * print_alphabet - print alphabet
  *
- * Return: Always 0 (Succes)
+ * Return: void
  */
 void print_alphabet(void)
 {
-	int n;
+	char letter = 'a';
 
-	for (n = 'a'; n <= 'z'; n++)
+	while (letter <= 'z')
 	{
-		_putchar(n);
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
 }
