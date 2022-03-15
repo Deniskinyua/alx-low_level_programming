053cb631e3d842a2add0cae21dc5e5ae7c50a493
Deniskinyua <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@github.com>
2022-03-16 01:49:34 +0300

    Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function printing 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int m, n;
+
+	n= 0;
+
+	while (n < 10)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
