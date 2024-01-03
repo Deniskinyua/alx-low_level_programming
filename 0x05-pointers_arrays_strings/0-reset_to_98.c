34069cf5ca78d20b35bbac62b7bb94c6b3e15ee2
Denis Kinyua <kimenjudenis@gmail.com>
2024-01-04 01:45:16 +0300

    Task 0 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer and updates the value it point to
+ * reset_to_98 - takes a pointer *n as parameter and updates the value it point to 98
  * @n: pointer
- * Return: void
+ * Return: no return because function is of void type
  */
 
 void reset_to_98(int *n)
