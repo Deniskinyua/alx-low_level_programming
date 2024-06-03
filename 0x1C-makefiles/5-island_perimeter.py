152f2e8019ab88c5294935731e3ccd20198c0ea5
Denis Kinyua <kimenjudenis@gmail.com>
2024-06-03 04:56:23 +0300

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Technical Interview Prep: Island Parameter"""
+
+def island_perimeter(grid):
+    w = len(grid[0])
+    h = le(grid)
+    ed = 0
+    sz = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                sz += 1
+                if(j > 0 and grid[i][j - 1] == 1):
+                    ed += 1
+                if(i > 0 and grid [i][i - 1] == 1):
+                    ed += 1
+    return sz * 4 - ed * 2
